6101380526a6a5d1fb3e1eed1007df5ce3bc1ca5
youngjerald239 <youngjerald239@gmail.com>
2022-09-10 18:40:44 -0500

    set up game scene

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import Phaser from './lib/phaser.js'
 
-console.dir(Phaser)
+import Game from './scenes/Game.js'
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+    scene: Game
+})
 
