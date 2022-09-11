3ff5dc613ea03f2f45acfa13b64123468e3305f4
youngjerald239 <youngjerald239@gmail.com>
2022-09-10 19:31:23 -0500

    added scrolling platforms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,6 +1,9 @@
 import Phaser from "../lib/phaser.js";
 
 export default class Game extends Phaser.Scene{
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    player
+
     constructor(){
         super('game')
     }
@@ -11,6 +14,9 @@ export default class Game extends Phaser.Scene{
 
         // load the platform image
         this.load.image('platform', 'assets/ground_grass.png')
+
+        // load the player image
+        this.load.image('wizkid_stand', 'assets/wizbizstand.png')
     }
 
     create()
@@ -37,6 +43,30 @@ export default class Game extends Phaser.Scene{
             /** @type {Phaser.Physics.Arcade.StaticBody} */
             const body = platform.body
             body.updateFromGameObject()
+        }
+        // add player sprite
+        this.player = this.physics.add.sprite(240, 320, 'wizkid_stand')
+            .setScale(0.5)
+        this.physics.add.collider(platforms, this.player)
+
+        // Collision for sprite
+        this.player.body.checkCollision.up = false
+        this.player.body.checkCollision.left = false
+        this.player.body.checkCollision.right = false
+
+        // Camera follows the player
+        this.cameras.main.startFollow(this.player)
+    }
+
+    update()
+    {
+        // find out from Arcade Physics if the player's physics body
+        // is touching something below it
+        const touchingDown = this.player.body.touching.down
+        if (touchingDown)
+        {
+            // this makes wizkid jump straight up
+            this.player.setVelocityY(-300)
         }
     }
 }
